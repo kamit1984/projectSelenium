c15e554ca68fe0b08a01e2efc071fdff88f50830
Amit Kshirsagar <kamit1984@gmail.com>
2020-09-17 21:34:14 +0200

    Updated pom file. Added properties file. Added initial draft of testcase in SampleTest.java.

---
+++ src/test/java/com/eqs/tmp/tmp.java
@@ -0,0 +1,13 @@
+package com.eqs.tmp;
+
+/**
+ * Create by Amit on 9/17/20
+ */
+public class tmp {
+
+    public static void main(String[] args) {
+
+        String x = "$16.40";
+        System.out.println(x.replaceAll("\\$",""));
+    }
+}
